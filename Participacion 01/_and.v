c786b54a57d8adafd3eead8734035f84fffdd741
liliabetancourt0320 <lilia.betancourt0320@alumnos.udg.mx>
2025-08-18 19:30:39 -0600

    Add files via upload

---
+++ Participacion 01/_and.v
@@ -0,0 +1,10 @@
+//Definir modulo y uss i/o
+module _and (input A, input B, output C);
+//2. Declrar señales/elementos internos
+//NA
+//3. Comportamiento del modulo 
+	//asignaciones,instancias, conexiones
+assign C=A&B;
+
+
+endmodule;
